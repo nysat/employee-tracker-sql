489512ab2bea9e69a6c8184c694d629db563fea8
nysat <nysa-cere3@hotmail.com>
2023-09-22 20:20:41 -0400

    added functions to server.js and added a .env file

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
--- creating database
+-- creating database--
 DROP DATABASE IF EXISTS empltrackerDB;
 CREATE DATABASE empltrackerDB;
 USE empltrackerDB;
- --department table--
+ -- department table--
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30)
 );
---roles table--
+-- roles table--
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
@@ -15,13 +15,13 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
---employee table-- 
+-- employee table-- 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role _id) REFERENCES role(id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const inquirer = require ("inquirer");
 const mysql = require("mysql2");
+const cTable = require('console.table');
+const env = require('dotenv').config()
 
-
-//creating a connection object '
-const URI = `mysql2://root:milothecat@127.0.0.0:3306/employee-tracker`
+//creating a connection object using dotenv
+const URI = process.env.MYSQLURI
 
 connection.connect((err) => {
     if (err) throw err;
@@ -57,3 +58,83 @@ function start() {
 };
 
 //VIEW ALL EMPLOYEES OPTIION
+function viewAllEmployees() {
+    const query = `
+    SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+    FROM employee e
+    LEFT JOIN roles r ON e.role_id = r.id
+    LEFT JOIN departments d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id;
+    `;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start(); //restarts application 
+    });
+}
+
+//ADD EMPLOYEE 
+function addEmployee() { 
+    inquirer.prompt([
+        {
+          name: "firstname",
+          type: "input",
+          message: "Enter their first name "
+        },
+        {
+          name: "lastname",
+          type: "input",
+          message: "Enter their last name "
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is their role? ",
+          choices: selectRole()
+        },
+        {
+            name: "choice",
+            type: "rawlist",
+            message: "Whats their managers name?",
+            choices: selectManager()
+        }
+    ]).then(function (val) {
+      var roleId = selectRole().indexOf(val.role) + 1
+      var managerId = selectManager().indexOf(val.choice) + 1
+      connection.query("INSERT INTO employee SET ?", 
+      {
+          first_name: val.firstName,
+          last_name: val.lastName,
+          manager_id: managerId,
+          role_id: roleId
+          
+      }, function(err){
+          if (err) throw err
+          console.table(val)
+          startPrompt()
+      })
+
+  })
+}
+
+//VIEW ALL ROLES
+function viewRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    startPrompt()
+    })
+  }
+
+//VIEW ALL DEPARTMENTS
+function viewAllDepartments() {
+    const query = "SELECT * FROM departments";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        // restart the application
+        start();
+    });
+};
+
